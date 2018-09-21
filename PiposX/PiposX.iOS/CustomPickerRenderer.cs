ef67af88de6bfa0096ca003756933ca05f01e283
virendar kumar <virendarkumar777@gmail.com>
2018-09-21 17:58:06 +0530

    language change

---
+++ PiposX/PiposX.iOS/CustomPickerRenderer.cs
@@ -0,0 +1,27 @@
+﻿using PiposX.Controls;
+using PiposX.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomPicker), typeof(CustomPickerRenderer))]
+namespace PiposX.iOS
+{
+   
+    public class CustomPickerRenderer : PickerRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Picker> e)
+        {
+            base.OnElementChanged(e);
+
+            var element = (CustomPicker)this.Element;
+
+            if (this.Control != null && this.Element != null && !string.IsNullOrEmpty(element.Image))
+            {
+                var downarrow = UIImage.FromBundle(element.Image);
+                Control.RightViewMode = UITextFieldViewMode.Always;
+                Control.RightView = new UIImageView(downarrow);
+            }
+        }
+    }
+}
